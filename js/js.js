dbfc6044b08575bd9be9aac7cb208225ca423a26
dzmitrydabrahost <dzmitrydabrahost@gmail.com>
2020-11-10 10:13:03 +0300

    fix: complete task

--- js/js.js
+++ js/js.js
@@ -5,30 +5,30 @@ const prev = document.querySelector('.slider-arrows__left-img');
 const next = document.querySelector('.slider-arrows__right-img');
 const title = document.querySelector('.slider-content-title');
 const subtitle = document.querySelector('.slider-content-subtitle');
+const formAction = document.querySelector('.header-button');
 const sliderContainer = document.querySelector('.slider-content');
 let count = 1;
 const slideContent = [
-        {id: 1, title: 'Взыскание дебиторской задолженности', subtitle: ''},
-        {id: 2, title: 'Юридический аутсорсинг', subtitle: 'Гарантия своевременности и качества услуг'},
-        {id: 3, title: 'Юридическая консультация', subtitle: 'Детальные разъяснения вопросов по ведению бизнеса от профессионалов'},
-        {id: 4, title: 'Ликвидация предприятия', subtitle: 'Эффективное решение сложных задач'},
-        {id: 5, title: 'Регистрация ООО', subtitle: 'Гарантируем мин имальные сроки регистрации под ключ '}
+        {id: 1, title: 'Взыскание дебиторской задолженности', subtitle: '', src: '#'},
+        {id: 2, title: 'Юридический аутсорсинг', subtitle: 'Гарантия своевременности и качества услуг', src: '#'},
+        {id: 3, title: 'Юридическая консультация', subtitle: 'Детальные разъяснения вопросов по ведению бизнеса от профессионалов', src: '#'},
+        {id: 4, title: 'Ликвидация предприятия', subtitle: 'Эффективное решение сложных задач', src: '#'},
+        {id: 5, title: 'Регистрация ООО', subtitle: 'Гарантируем мин имальные сроки регистрации под ключ', src: '#'}
 ];
 
 const changeSlider = () => {
     sliderContainer.classList.remove('show-block');
     sliderContainer.classList.add('hide-block');
-    setInterval(() => {
     slideContent.forEach((item) => {
         if(item.id === count) {
             slider.style.backgroundImage = `url(../../assets/images/slides/slider${count}.png)`;
             title.textContent = `${item.title}`;
             subtitle.textContent = `${item.subtitle}`;
+            formAction.action = `${item.src}`; 
         }
     });
         sliderContainer.classList.remove('hide-block');
         sliderContainer.classList.add('show-block');
-    }, 2000);
 }
 
 slider.addEventListener('click', (e) => {
@@ -53,23 +53,27 @@ const viewport = document.querySelector('.slide-content-image').offsetWidth;
 const btnNext = document.querySelector('.slide-content-arrow-right-item');
 const btnPrev = document.querySelector('.slide-content-arrow-left-item');
 const sliderBlock = document.querySelector('.slide-content-image-block');
+const sliderItems = document.querySelectorAll('.slide-content-image-item');
+
 let viewSlide = 0;
  
-btnNext.addEventListener('click', (e) => {
+btnNext.addEventListener('click', () => {
     btnPrev.classList.remove('inactivity');
-    if (viewSlide < 4) { 
+    if (viewSlide < sliderItems.length - 2) { 
         viewSlide++;
-    } else {
+    } else if (viewSlide < sliderItems.length - 1) {
+        viewSlide++;
         btnNext.classList.add('inactivity');
     }
     sliderBlock.style.left = `-${viewSlide * viewport}px`;
 });
  
-btnPrev.addEventListener('click', function () {
+btnPrev.addEventListener('click', () => {
     btnNext.classList.remove('inactivity');
-    if (viewSlide > 0) { 
+    if (viewSlide > 1) { 
         viewSlide--; 
-    } else { 
+    } else if (viewSlide === 1) { 
+        viewSlide--;
         btnPrev.classList.add('inactivity'); 
     }
     sliderBlock.style.left = `-${viewSlide * viewport}px`;
@@ -81,35 +85,33 @@ const nextBtn = document.querySelector('.reviews-card-buttons-arrows-right');
 const prevBtn = document.querySelector('.reviews-card-buttons-arrows-left');
 const parentBlock = document.querySelector('.reviews-card-block');
 const blockSlider = document.querySelectorAll('.reviews-card');
+const widthCard = document.querySelector('.reviews-card').offsetWidth;
 const pointArray = document.querySelectorAll('.reviews-card-buttons__point');
 let countSlide = 0;
 
-nextBtn.addEventListener('click', (e) => {
+nextBtn.addEventListener('click', () => {
     prevBtn.classList.remove('inactivity');
+    countSlide++;
+    if (countSlide === blockSlider.length - 2) {
+        nextBtn.classList.add('inactivity');
+    };
     removeActiveClass();
-    countSlide < blockSlider.length - 2 ? countSlide++ :  nextBtn.classList.add('inactivity');
-    parentBlock.style.left = `-${countSlide * (parentBlock.offsetWidth) / 2}px`;
-    console.log(countSlide, blockSlider.length)
+    parentBlock.style.left = `-${(countSlide * widthCard) + (countSlide * +window.getComputedStyle(parentBlock).columnGap.slice(0, 2))}px`;
 });
 
-prevBtn.addEventListener('click', (e) => {
+prevBtn.addEventListener('click', () => {
     nextBtn.classList.remove('inactivity');
+    countSlide--;
     removeActiveClass();
-    if (countSlide > 0) {
-        countSlide--;
-    } else {
+    if (countSlide === 0) {
         prevBtn.classList.add('inactivity');
-    }
-    console.log(countSlide, blockSlider.length)
-    parentBlock.style.left = `-${countSlide * (parentBlock.offsetWidth / 2)}px`;
-    console.dir(blockSlider);
+    };
+    parentBlock.style.left = `-${(countSlide * widthCard) + (countSlide * +window.getComputedStyle(parentBlock).columnGap.slice(0, 2))}px`;;
 });
 
 const removeActiveClass = () => {
-    pointArray.forEach((item, index) => {
+    pointArray.forEach((item) => {
         item.classList.remove('point-active');
-        if(index === countSlide + 1) {
-            item.classList.add('point-active');
-        }
     });
+    pointArray[countSlide].classList.add('point-active');
 };
